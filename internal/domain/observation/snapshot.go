dddb264bdb5b560559c0db6fdb4bb0f167343615
Daichi Suzuki <ds.81ueman@gmail.com>
2026-05-31 14:46:06 +0900

    Define canonical observation model (#117)

---
+++ internal/domain/observation/snapshot.go
@@ -0,0 +1,24 @@
+package observation
+
+type NetworkSnapshot struct {
+	Metadata SnapshotMetadata `json:"metadata"`
+	Nodes    []NodeSnapshot   `json:"nodes"`
+}
+
+type SnapshotMetadata struct {
+	ID          string            `json:"id,omitempty"`
+	Source      string            `json:"source,omitempty"`
+	CollectedAt string            `json:"collected_at,omitempty"`
+	Labels      map[string]string `json:"labels,omitempty"`
+}
+
+type NodeSnapshot struct {
+	Node NodeID        `json:"node"`
+	VRFs []VRFSnapshot `json:"vrfs"`
+}
+
+type VRFSnapshot struct {
+	VRF VRFName `json:"vrf"`
+	RIB RIB     `json:"rib"`
+	FIB FIB     `json:"fib"`
+}
